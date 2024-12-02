4999ef69fb355a69e72f06b914438ed8dd0bb630
Anuj Gupta <ganuj2674@gmail.com>
2024-12-02 16:14:09 +0530

    User Login and Register done

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,20 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 4000;
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+connectToDb();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
---
+++ Backend/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/uber').then(() => {
+        console.log("Connencted to DB");
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
+module.exports = connectToDb;
