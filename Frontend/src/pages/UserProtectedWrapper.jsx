ded80c7598f558f974fa115704f45e12f0cc825b
Anuj Gupta <ganuj2674@gmail.com>
2024-12-05 20:02:55 +0530

    Small Improvements

--- Frontend/src/pages/UserProtectedWrapper.jsx
+++ Frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserDataContext } from '../context/UserContext';
+import axios from 'axios';
 
 const UserProtectedWrapper = ({ children }) => {
     const navigate = useNavigate();
