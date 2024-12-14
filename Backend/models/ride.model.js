59745c7bab1fbdf8e45add7c818a4233b4b0ce51
Anuj Gupta <ganuj2674@gmail.com>
2024-12-14 23:08:41 +0530

    Working on ride routes of uber clone

---
+++ Backend/models/ride.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const rideSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
